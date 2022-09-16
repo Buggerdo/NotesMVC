9555edc118ff82ccaa920dd144d269b076150cb3
Troy <wilson.troy.anthony@gmail.com>
2022-09-15 21:26:00 -0400

    Add project files.

---
+++ NotesMVC/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesMVC.Models;
+using System.Diagnostics;
+
+namespace NotesMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private NotesMVCContext context = new NotesMVCContext();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CreateNote()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        public IActionResult AddNoteToDb(Note note)
+        {
+            note.Timestamp = DateTime.Now;
+            context.Notes.Add(note);
+            context.SaveChanges();
+            return RedirectToAction("GetNotes");
+        }
+
+        public IActionResult GetNotes()
+        {
+            return View(context.Notes.ToList());
+        }
+    }
+}
+
+//Scaffold-DbContext 'Data Source=TROYSPC;Initial Catalog=NotesMVC; Integrated Security=SSPI;' Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
---
+++ NotesMVC/Models/NotesMVCContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace NotesMVC.Models
+{
+    public partial class NotesMVCContext : DbContext
+    {
+        public NotesMVCContext()
+        {
+        }
+
+        public NotesMVCContext(DbContextOptions<NotesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Note> Notes { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=TROYSPC;Initial Catalog=NotesMVC; Integrated Security=SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>(entity =>
+            {
+                entity.Property(e => e.Content).HasMaxLength(1000);
+
+                entity.Property(e => e.Timestamp).HasColumnType("datetime");
+
+                entity.Property(e => e.Title).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
